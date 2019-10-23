6dd5db50d81cda7e876ec8d683b28fe848f6b681
Manul <kolupaeff13102001@yandex.ru>
2019-10-23 19:36:36 +0300

    Sergey Kolupaev /2

---
+++ src/lesson2/task1/IfElse.kt
@@ -0,0 +1,184 @@
+@file:Suppress("UNUSED_PARAMETER")
+
+package lesson2.task1
+
+import lesson1.task1.discriminant
+import kotlin.math.abs
+import kotlin.math.max
+import kotlin.math.min
+import kotlin.math.sqrt
+
+/**
+ * Пример
+ *
+ * Найти число корней квадратного уравнения ax^2 + bx + c = 0
+ */
+fun quadraticRootNumber(a: Double, b: Double, c: Double): Int {
+    val discriminant = discriminant(a, b, c)
+    return when {
+        discriminant > 0.0 -> 2
+        discriminant == 0.0 -> 1
+        else -> 0
+    }
+}
+
+/**
+ * Пример
+ *
+ * Получить строковую нотацию для оценки по пятибалльной системе
+ */
+fun gradeNotation(grade: Int): String = when (grade) {
+    5 -> "отлично"
+    4 -> "хорошо"
+    3 -> "удовлетворительно"
+    2 -> "неудовлетворительно"
+    else -> "несуществующая оценка $grade"
+}
+
+/**
+ * Пример
+ *
+ * Найти наименьший корень биквадратного уравнения ax^4 + bx^2 + c = 0
+ */
+fun minBiRoot(a: Double, b: Double, c: Double): Double {
+    // 1: в главной ветке if выполняется НЕСКОЛЬКО операторов
+    if (a == 0.0) {
+        if (b == 0.0) return Double.NaN // ... и ничего больше не делать
+        val bc = -c / b
+        if (bc < 0.0) return Double.NaN // ... и ничего больше не делать
+        return -sqrt(bc)
+        // Дальше функция при a == 0.0 не идёт
+    }
+    val d = discriminant(a, b, c)   // 2
+    if (d < 0.0) return Double.NaN  // 3
+    // 4
+    val y1 = (-b + sqrt(d)) / (2 * a)
+    val y2 = (-b - sqrt(d)) / (2 * a)
+    val y3 = max(y1, y2)       // 5
+    if (y3 < 0.0) return Double.NaN // 6
+    return -sqrt(y3)           // 7
+}
+
+/**
+ * Простая
+ *
+ * Мой возраст. Для заданного 0 < n < 200, рассматриваемого как возраст человека,
+ * вернуть строку вида: «21 год», «32 года», «12 лет».
+ */
+fun ageDescription(age: Int) {
+    var result: String = when {
+        (age % 100 >= 5) && (age % 100 <= 20) -> "$age лет"
+        (age % 10 == 1) -> "$age год"
+        ((age % 10 == 2) || (age % 10 == 3) || (age % 10 == 4)) -> "$age года"
+        else -> "$age лет"
+    }
+
+}
+
+/**
+ * Простая
+ *
+ * Путник двигался t1 часов со скоростью v1 км/час, затем t2 часов — со скоростью v2 км/час
+ * и t3 часов — со скоростью v3 км/час.
+ * Определить, за какое время он одолел первую половину пути?
+ */
+fun timeForHalfWay(
+    t1: Double, v1: Double,
+    t2: Double, v2: Double,
+    t3: Double, v3: Double
+): Double {
+
+    var s1 = t1 * v1
+    var s2 = t2 * v2
+    var s3 = t3 * v3
+    var s = (s1 + s2 + s3) / 2
+    return when {
+        s == s1 -> t1
+        (s < s1) -> s / v1
+        (s > s1) && (s < s2 + s1) -> t1 + ((s - s1) / v2)
+        (s == s1 + s2) -> t1 + t2
+        (s > s2 + s1) && (s < s3 + s2 + s1) -> t1 + t2 + ((s - s2 - s1) / v3)
+        else -> t1 + t2 + t3
+
+    }
+}
+
+/**
+ * Простая
+ *
+ * Нa шахматной доске стоят черный король и две белые ладьи (ладья бьет по горизонтали и вертикали).
+ * Определить, не находится ли король под боем, а если есть угроза, то от кого именно.
+ * Вернуть 0, если угрозы нет, 1, если угроза только от первой ладьи, 2, если только от второй ладьи,
+ * и 3, если угроза от обеих ладей.
+ * Считать, что ладьи не могут загораживать друг друга
+ */
+fun whichRookThreatens(
+    kingX: Int, kingY: Int,
+    rookX1: Int, rookY1: Int,
+    rookX2: Int, rookY2: Int
+) {
+    var x = kingX == rookX1
+    var z = kingX == rookX2
+    var y = kingY == rookY1
+    var f = kingX == rookX2
+    var t = kingY == rookY2
+    when {
+        ((x) || (y)) && ((f) || (t)) -> 3
+        (x) || (y) -> 1
+        (z) || (t) -> 2
+        else -> 0
+
+    }
+}
+
+
+/**
+ * Простая
+ *
+ * На шахматной доске стоят черный король и белые ладья и слон
+ * (ладья бьет по горизонтали и вертикали, слон — по диагоналям).
+ * Проверить, есть ли угроза королю и если есть, то от кого именно.
+ * Вернуть 0, если угрозы нет, 1, если угроза только от ладьи, 2, если только от слона,
+ * и 3, если угроза есть и от ладьи и от слона.
+ * Считать, что ладья и слон не могут загораживать друг друга.
+ */
+fun rookOrBishopThreatens(
+    kingX: Int, kingY: Int,
+    rookX: Int, rookY: Int,
+    bishopX: Int, bishopY: Int
+): Int = when {
+    ((kingX == rookX) || (kingY == rookY)) || (abs(kingX - bishopX) == abs(kingY - bishopY)) -> 3
+    (kingX == rookX) || (kingY == rookY) -> 1
+    (abs(kingX - bishopX) == abs(kingY - bishopY)) -> 2
+    else -> 0
+}
+
+/**
+ * Простая
+ *
+ * Треугольник задан длинами своих сторон a, b, c.
+ * Проверить, является ли данный треугольник остроугольным (вернуть 0),
+ * прямоугольным (вернуть 1) или тупоугольным (вернуть 2).
+ * Если такой треугольник не существует, вернуть -1.
+ */
+fun triangleKind(a: Double, b: Double, c: Double): Int = when {
+    (c >= a + b) || (a >= b + c) || (b >= a + c) -> -1
+    (c * c == a * a + b * b) || (a * a == b * b + c * c) || (b * b == a * a + c * c) -> 1
+    (c * c < a * a + b * b) && (a * a < b * b + c * c) && (b * b < a * a + c * c) -> 0
+    else -> 2
+
+
+}
+
+/**
+ * Средняя
+ *
+ * Даны четыре точки на одной прямой: A, B, C и D.
+ * Координаты точек a, b, c, d соответственно, b >= a, d >= c.
+ * Найти длину пересечения отрезков AB и CD.
+ * Если пересечения нет, вернуть -1.
+ */
+fun segmentLength(a: Int, b: Int, c: Int, d: Int): Int = when {
+    min(b, d) - max(a, c) >= 0 -> min(b, d) - max(a, c)
+    else -> -1
+}
